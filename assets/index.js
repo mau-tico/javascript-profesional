a1fe2a440694a6de05010e1aed4b4c25157f9e14
mau <mauriciorj76@gmail.com>
2021-05-09 20:58:39 -0600

    Un vergo de varas

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,18 @@
   import MediaPlayer from './MediaPlayer.js';
   import AutoPlay from './plugins/AutoPlay.js'
+  import AutoPause from './plugins/AutoPause.js';
 
   const video = document.querySelector('video');
   const player = new MediaPlayer({ 
     el: video,
-    plugins: [new AutoPlay()] 
+    plugins: [new AutoPlay(), new AutoPause()] 
   });
 
   const button = document.querySelector('button');
   button.onclick = () => player.togglePlay();
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+      
+    })
+  };
